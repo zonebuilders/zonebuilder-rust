5fafa001341858c9146669333d6f72ae25bc53ea
Robin Lovelace <rob00x@gmail.com>
2021-03-20 23:29:27 +0000

    Make more params defaults, fix #6

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub struct Params {
     n_circles: usize,
     num_segments: usize,
     distances: Vec<f64>,
+    num_vertices: usize
 }
 
 // https://doc.rust-lang.org/std/default/trait.Default.html
@@ -16,33 +17,23 @@ impl Default for Params {
         // todo: distances should be:
         // zonebuilder::zb_100_triangular_numbers
         // 1    3    6   10   15   21   28   36   45   55   66 ...
-        Params { n_circles: 5, num_segments: 12, distances: vec![1.0, 3.0, 6.0, 10.0, 15.0]}
+        Params { n_circles: 5, num_segments: 12, distances: vec![1.0, 3.0, 6.0, 10.0, 15.0], num_vertices: 121}
     }
 }
 
-
 pub fn clockboard(
     centerpoint: Point<f64>,
-    num_segments: usize,
-    radii: Vec<f64>,
+    params: Params,
     boundary: Option<Polygon<f64>>,
-    params: Params
 ) -> Vec<Polygon<f64>> {
-    // test options worked // no idea if this next line is useful (probably not)
-    // let options = Params {n_circles: 42 ..Default::Default} 
-    println!("{:?}", params.n_circles);
     let mut polygons = Vec::new();
-    let circle = makecircle(centerpoint, radii[0]);
+    let circle = makecircle(centerpoint, params.distances[0], params.num_vertices);
     polygons.push(circle);
     polygons
 }
 
-fn makecircle(centerpoint: Point<f64>, radius: f64) -> Polygon<f64> {
-    // hardcode num vertices - can be argument later
-    let num_vertices = 121;
-
+fn makecircle(centerpoint: Point<f64>, radius: f64, num_vertices: usize) -> Polygon<f64> {
     let mut circle_points = Vec::new();
-
     // in R: 1:num_vertices
     for i in 0..num_vertices {
         let angle: f64 = 2.0 * std::f64::consts::PI / (num_vertices as f64) * (i as f64);
@@ -50,8 +41,6 @@ fn makecircle(centerpoint: Point<f64>, radius: f64) -> Polygon<f64> {
         let y = centerpoint.y() + radius * angle.sin();
         circle_points.push(Point::new(x, y));
     }
-
     let polygon = Polygon::new(LineString::from(circle_points), vec![]);
-
     polygon
 }
