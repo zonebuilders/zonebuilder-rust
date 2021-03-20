f7380b129984ac909564773101e2e5b2d6384460
Robin Lovelace <rob00x@gmail.com>
2021-03-20 17:30:45 +0000

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
-use geo::{Point, LineString, Polygon};
+use geo::{LineString, Point, Polygon};
 
-pub fn clockboard(centerpoint: Point<f64>, num_segments: usize, radii: Vec<f64>, boundary: Option<Polygon<f64>>) -> Vec<Polygon<f64>> {
-
+pub fn clockboard(
+    centerpoint: Point<f64>,
+    num_segments: usize,
+    radii: Vec<f64>,
+    boundary: Option<Polygon<f64>>,
+) -> Vec<Polygon<f64>> {
     let mut polygons = Vec::new();
     let circle = makecircle(centerpoint, radii[0]);
-    println!("{:?}", circle);
+    //println!("{:?}", circle);
     polygons.push(circle);
-    println!("{:?}", polygons[0]);
-    polygons 
-   
+    //println!("{:?}", polygons[0]);
+    polygons
 }
 
 fn makecircle(centerpoint: Point<f64>, radius: f64) -> Polygon<f64> {
-
     // hardcode num vertices - can be argument later
     let num_vertices = 121;
 
@@ -26,10 +28,7 @@ fn makecircle(centerpoint: Point<f64>, radius: f64) -> Polygon<f64> {
         circle_points.push(Point::new(x, y));
     }
 
-    let polygon = Polygon::new(
-    LineString::from(circle_points),
-    vec![],
-    );    
+    let polygon = Polygon::new(LineString::from(circle_points), vec![]);
 
     polygon
 }
@@ -39,7 +38,6 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        
         clockboard(Point::new(0.0, 0.0), 2, vec![1.0], None);
         assert!(false)
     }
