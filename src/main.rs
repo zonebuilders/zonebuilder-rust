4cc2c779f853d851b3b5ddc30c4dcf6d59d10df0
Robin Lovelace <rob00x@gmail.com>
2021-03-20 22:59:23 +0000

    Try with default ..Params::default()

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use geo::Point;
 use zonebuilder::clockboard;
 
 fn main() {
-    let polygon_list = clockboard(Point::new(0.0, 0.0), 2, vec![1.0], None);
+    let polygon_list = clockboard(Point::new(0.0, 0.0), 2, vec![1.0], None, ..Params::default());
     let geojson_list = geojson::Value::from(&polygon_list[0]);
     println!("{}", geojson_list);
 }
