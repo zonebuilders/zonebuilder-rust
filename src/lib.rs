f33e1c22485dbd749d11d8daa101e10f7e94801c
Robin Lovelace <rob00x@gmail.com>
2021-03-20 16:25:25 +0000

    Add geo dependency, add function arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use geo::{Point, Polygon};
+
+pub fn clockboard(centerpoint: Point<f64>, num_segments: usize, radii: Vec<f64>, boundary: Option<Polygon<f64>>) -> Vec<Polygon<f64>> {
+   Vec::new() 
+}  
+
 #[cfg(test)]
 mod tests {
     #[test]
