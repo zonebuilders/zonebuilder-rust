39b5f71679f4f25edd6ef06966051b552a3a6a66
Robin Lovelace <rob00x@gmail.com>
2021-03-21 22:12:43 +0000

    First pass for progress on #5

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 use geo::Point;
-use zonebuilder::Params;
-use zonebuilder::clockboard;
+use zonebuilder::{clockboard, Params};
+use clap::{Arg, App};
 
 fn main() {
+    let clockboard: clap::ArgMatches = App::new("My Test Program")
+    .version("0.1.0")
+    .author("zonebuilders")
+    .about("Builds zones")
+    .arg(Arg::with_name("num_vertices")
+             .short("n")
+             .long("number")
+             .takes_value(true)
+             .help("Number of vertices"))
+    .clockboard();
+
+    let num_vertices = matches.value_of("num_vertices").unwrap_or("input.txt");
+
     let polygon_list = clockboard(Point::new(0.0, 0.0), Params::default(), None);
     let geojson_list = geojson::Value::from(&polygon_list[0]);
     println!("{}", geojson_list);
