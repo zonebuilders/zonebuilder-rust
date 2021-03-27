c44accde8d3dada4437734b0f89ab37acebf940c
Robin Lovelace <rob00x@gmail.com>
2021-03-27 20:48:52 +0000

    Format

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use geo::{LineString, Point, Polygon, coords_iter::CoordsIter, map_coords::MapCoordsInplace};
+use geo::{coords_iter::CoordsIter, map_coords::MapCoordsInplace, LineString, Point, Polygon};
 use std::default::Default;
 
 // See https://stackoverflow.com/questions/24047686
@@ -8,7 +8,7 @@ pub struct Params {
     num_segments: usize,
     distances: Vec<f64>,
     num_vertices: usize,
-    precision: usize
+    precision: usize,
 }
 
 // https://doc.rust-lang.org/std/default/trait.Default.html
@@ -18,13 +18,21 @@ impl Default for Params {
         // todo: distances should be:
         // zonebuilder::zb_100_triangular_numbers
         // 1    3    6   10   15   21   28   36   45   55   66 ...
-        Params { n_circles: 5, num_segments: 12, distances: vec![1.0, 3.0, 6.0, 10.0, 15.0], num_vertices: 121, precision: 5}
+        Params {
+            n_circles: 5,
+            num_segments: 12,
+            distances: vec![1.0, 3.0, 6.0, 10.0, 15.0],
+            num_vertices: 121,
+            precision: 5,
+        }
     }
 }
 
 fn round(poly: &mut Polygon<f64>, precision: usize) {
     // hardcoded 2 d.p. todo: update
-    poly.map_coords_inplace(|&(x, y)| (f64::trunc(x  * 100.0) / 100.0, f64::trunc(y  * 100.0) / 100.0))    
+    poly.map_coords_inplace(|&(x, y)| {
+        (f64::trunc(x * 100.0) / 100.0, f64::trunc(y * 100.0) / 100.0)
+    })
 }
 
 pub fn clockboard(
@@ -33,7 +41,7 @@ pub fn clockboard(
     boundary: Option<Polygon<f64>>,
 ) -> Vec<Polygon<f64>> {
     let mut polygons = Vec::new();
-    let circle = makecircle(centerpoint, params.distances[0], params.num_vertices); 
+    let circle = makecircle(centerpoint, params.distances[0], params.num_vertices);
     polygons.push(circle);
     polygons
 }
