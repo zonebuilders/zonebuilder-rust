1cf799aefab27f33701a6a3d830d4f480534ae00
Robin Lovelace <rob00x@gmail.com>
2021-03-27 22:10:30 +0000

    Try with multiple polygons - compile issue

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use zonebuilder::Params;
 
 fn main() {
     let polygon_list = clockboard(Point::new(0.0, 0.0), Params::default(), None);
-    let geojson_list = geojson::Value::from(&polygon_list[0]);
+    let geojson_list = geojson::Value::from(&polygon_list);
     println!("{}", geojson_list);
 }
