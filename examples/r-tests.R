a715643e8267bb3bf590d52dcb92480ee0a060d4
Robin Lovelace <rob00x@gmail.com>
2021-05-01 07:54:34 +0100

    Add R example

---
+++ examples/r-tests.R
@@ -0,0 +1,8 @@
+setwd("~/github-orgs/zonebuilders/zonebuilder-rust/")
+system("cargo run --example clockboard-example > zones_uk.geojson")
+zones = sf::read_sf("zones_uk.geojson")
+plot(zones)
+# interactive version with basemap
+library(tmap)
+tmap_mode("view")
+qtm(zones)
