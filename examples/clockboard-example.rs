34438a38bec06010db5d0f4eb57cb6cb2c7f9e9d
Robin Lovelace <rob00x@gmail.com>
2021-05-01 07:22:48 +0100

    Add working example, close #23

--- examples/clockboard-example.rs
+++ examples/clockboard-example.rs
@@ -1,6 +1,13 @@
-use std::env;
+// See https://github.com/zonebuilders/zonebuilder-rust/issues/23
+extern crate zonebuilder;
+use zonebuilder::Params;
+use structopt::StructOpt;
+use geo::Point;
+use serde_json::to_string_pretty;
 
 fn main() {
-    let name = env::args().skip(1).next();
-    println!("Hello, {}!", name.unwrap_or("world".into()));
+    let cp = Point::new(-1.5, 53.8);
+    let gj = zonebuilder::clockboard(cp, Params::from_args());
+    let gjstring = to_string_pretty(&gj).unwrap();
+    println!("{}", gjstring);
 }
